ff4f8744d7f1e7e0215cbbc29081236af0f25c50
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-01 00:12:47 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .ImagePreviewWithWebhook import ImagePreviewWithWebhook 
+
+NODE_CLASS_MAPPINGS = {
+    "ImagePreviewWithWebhook": ImagePreviewWithWebhook
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImagePreviewWithWebhook": "Image Preview with Webhook"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
